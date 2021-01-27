d17f5309a756308c0e2877c06de49d50d2663d08
tnjensen <thomasnorupjensen@gmail.com>
2021-01-27 11:20:01 +0100

    Added new index.html and stylesheet code for dropdown menus

--- js/script.js
+++ js/script.js
@@ -1,7 +1,53 @@
-var links = document.querySelector('.main-nav a [href="'+document.URL+'"]');
+var links = document.querySelector('nav a [href="'+document.URL+'"]');
 
-for (var i = 1; i < document.links.length; i++) {
+for (var i = 0; i < document.links.length; i++) {
     if (document.links[i].href == document.URL) {
         document.links[i].className = 'active';
     }
 }
+
+$(document).ready(function(){
+  var arrowLogin = $(".arrow-up-login");
+  var arrowRegister = $(".arrow-up-register");
+  var arrowCart = $(".arrow-up-cart");
+  var login = $(".login-form");
+  var register = $(".register-form");
+  var cart = $(".cart-form");
+  var status = false;
+  $("#login").click(function(event){
+    event.preventDefault();
+    if(status == false){
+      arrowLogin.fadeIn();
+      login.fadeIn();
+      status = true;
+    }else{
+      arrowLogin.fadeOut();
+      login.fadeOut();
+      status = false;
+    }
+  })
+  $("#register").click(function(event){
+    event.preventDefault();
+    if(status == false){
+      arrowRegister.fadeIn();
+      register.fadeIn();
+      status = true;
+    }else{
+      arrowRegister.fadeOut();
+      register.fadeOut();
+      status = false;
+    }
+  })
+  $("#cart").click(function(event){
+    event.preventDefault();
+    if(status == false){
+      arrowCart.fadeIn();
+      cart.fadeIn();
+      status = true;
+    }else{
+      arrowCart.fadeOut();
+      cart.fadeOut();
+      status = false;
+    }
+  })
+})
