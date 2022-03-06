c53e62c933100263045f6f9990ddc9ebaabcb638
tnjensen <thomasnorupjensen@gmail.com>
2022-03-06 22:01:32 +0100

    Edited secondary nav menu

--- js/script.js
+++ js/script.js
@@ -1,38 +1,35 @@
 var links = document.querySelector('nav a [href="'+document.URL+'"]');
+const dropItem = document.querySelectorAll('.has-dropdown');
 
 for (var i = 0; i < document.links.length; i++) {
     if (document.links[i].href == document.URL) {
         document.links[i].className = 'active';
     }
 }
-//This gets the entire html with tags and doctype
-/* var doc = new XMLSerializer().serializeToString(document); */
-var markup = document.documentElement.outerHTML;
-/* console.log(markup); */
-var searchBtn = document.querySelector('.fa-search');
 
-/* console.log(searchInput); */
-var searchTerm = document.querySelector('.search-term');
-var searchResult = document.querySelector('.search-results');
+dropItem.forEach( item =>{
+  item.addEventListener('click', function(event){
+    item.classList.toggle('visible');
 
-function displaySearch(){
-  var searchInput = document.getElementById('searchInput').value;
-  /* alert(searchInput); */
-  /* console.log(searchInput); */
+    // Ensure only one dropdown open 
+    for (var i = 0; i < dropItem.length; i++) {
+
+      // If the item is the one clicked, skip it
+      if (dropItem[i] === event.target) continue;
   
-  searchResult.innerHTML = searchInput;
-  searchTerm.innerHTML = `"${searchInput}"`;
-}
-searchBtn.onclick = displaySearch;
+      dropItem[i].classList.remove('visible');
+    }
+  })
+});
 
-  /* for(let i = 0; i < markup.length; i++){
-    if(!markup[i].innerHTML.toLowerCase.includes(searchInput)){
-      searchResult.innerHTML = "";
-    }
-    else{
-      searchResult.innerHTML = `<div class="search-results>${doc[i]}</div>">`;
-    }
-  } */
+/* Close dropdown when clicking outside */
+window.addEventListener('mouseup', function(event){
+   if(!event.target.closest('.secondary-nav')){
+        for (var i = 0; i < dropItem.length; i++) {
+        dropItem[i].classList.remove('visible');
+        }
+      }
+})
 
 /* $(document).ready(function(){
   var arrowLogin = $(".arrow-up-login");
